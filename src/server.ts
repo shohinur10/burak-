f94c594b5fc5d7d9cc51ee5132f51f39faad923a
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-03-12 23:34:10 +0900

    feat: integration of environmental variable

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,49 @@
 
-import moment from "moment";
+// import moment from "moment";
 
-const currTime = moment().format("YYY MM DD")
-console.log(currTime)
+// const currTime = moment().format("YYY MM DD")
+// console.log(currTime)
 
-const myName: string = "Shohinur";
-const age: number = 26;
+// const myName: string = "Shohinur";
+// const age: number = 26;
 
-console.log(myName)
-console.log(age)
+// console.log(myName)
+// console.log(age)
 
-interface Bank {
-    name: string,
-    started: number,
-    branch: number,
-    employees: number,
-    motto: string[],
-    logo: string,
-    ravenue: number,
-}
+// interface Bank {
+//     name: string,
+//     started: number,
+//     branch: number,
+//     employees: number,
+//     motto: string[],
+//     logo: string,
+//     ravenue: number,
+// }
 
-const wise: Bank = {
-    name: "wise",
-    started: 1990,
-    branch: 34,
-    employees: 1990,
-    motto: ['our bank'],
-    logo: "https/something",
-    ravenue: 1.8
-}
+// const wise: Bank = {
+//     name: "wise",
+//     started: 1990,
+//     branch: 34,
+//     employees: 1990,
+//     motto: ['our bank'],
+//     logo: "https/something",
+//     ravenue: 1.8
+// }
 
-console.log(`this bank is ${wise.name}, and it originally started in ${wise.started}. Now it has ${wise.branch}, ${wise.employees}, with ravenue of ${wise.ravenue}`)
+// console.log(`this bank is ${wise.name}, and it originally started in ${wise.started}. Now it has ${wise.branch}, ${wise.employees}, with ravenue of ${wise.ravenue}`)
 
 
-//architectual patters: MVC, DI, frontendga -> MVP
+// //architectual patters: MVC, DI, frontendga -> MVP
 
-//MVC - MODEL VIEW CONTROLLER
+// //MVC - MODEL VIEW CONTROLLER
 
-//design paterns: middlewares, decorators 
+// //design paterns: middlewares, decorators 
+
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// console.log("PORT:", process.env.PORT);
+
+// console.log("{MONGO_URL:", process.env.MONGO_URL);
