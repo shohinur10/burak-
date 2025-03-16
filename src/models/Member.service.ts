be0b9598cd9dca74176bac3833ad88b9545573d2
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-03-16 15:19:59 +0900

    feat :member service model  integration

---
+++ src/models/Member.service.ts
@@ -0,0 +1,5 @@
+class MemberService {
+    constructor(){}
+}
+// nma uchun biz MemberService katta harfda yozvolyapmiz chunkin biz doim classlardi pascal case orqali qurib olamiz 
+export default MemberService;
