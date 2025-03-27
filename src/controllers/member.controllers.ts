1cf1f8278618b6a6e8394a3a693815465bf349aa
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-03-27 14:53:59 +0900

    feat : organize SPA and SSR

--- src/controllers/member.controllers.ts
+++ src/controllers/member.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import { MemberInput,Member,LoginInput } from "../libs/types/member";
 import MemberService from "../models/Member.service";
+import Errors from "../libs/Errors";
 
 // REACT uchun ishleydu togrirogi SPA
 
@@ -15,11 +16,13 @@ memberController.signup = async (req: Request, res: Response) => {
     console.log("signup");
    const input : MemberInput = req.body,
      result: Member  = await memberService.signup(input);
-
+// TODO : Tokens
 
     res.json({member: result});
   } catch (err) {
     console.log("Error, signup:", err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
     // res.json({});
     
   }
@@ -30,10 +33,13 @@ memberController.login = async (req: Request, res: Response) => {
     console.log("login");
     const input: LoginInput = req.body,
     result = await memberService.login(input);
+  // TODO : Tokens
   
     res.json({member: result});
   } catch (err) {
     console.log("Error, login:", err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
  //   res.json({})
   }
 };
