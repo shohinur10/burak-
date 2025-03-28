fc2ab7641915c91d82ebdd8cdac2fe956f4ae89c
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-03-28 16:59:10 +0900

    feat : check errors

--- src/controllers/member.controllers.ts
+++ src/controllers/member.controllers.ts
@@ -23,7 +23,7 @@ memberController.signup = async (req: Request, res: Response) => {
     console.log("Error, signup:", err);
     if(err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
-    // res.json({});
+
     
   }
 };
