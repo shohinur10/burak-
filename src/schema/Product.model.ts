ad7187ea2d1fe24a61d704c5552c17796731e9c5
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-03-30 15:18:40 +0900

    feat : making enum and service models

--- src/schema/Product.model.ts
+++ src/schema/Product.model.ts
@@ -1,29 +1,65 @@
-import mongoose, {Schema} from "mongoose";
-import { ProductCollection, ProductStatus } from '../libs/enums/product.enum';
-
-
+import mongoose, { Schema } from "mongoose";
+import { 
+    ProductCollection,
+    ProductSize, 
+    ProductStatus,
+} from '../libs/enums/product.enum';
+import { ProductVaolume } from "../libs/enums/product.enum";
 
 const productSchema = new Schema(
-{
- productStatus: {
-    type: String,
-    enum: ProductCollection,
-    required: true,
- },
-   productCollection: {
-    type:String ,
-    enum: ProductCollection,
-    required:true,
-
- },
-
-productName:{
-    type:String,
-    required:true, 
-},
-},
-{ timestamps: true} // updatedAt, createdAt
+  {
+    productStatus: {
+      type: String,
+      enum: ProductStatus, // FIXED
+      required: true,
+    },
+    productCollection: {
+      type: String,
+      enum: ProductCollection,
+      required: true,
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+    productLeftCount: {
+      type: Number,
+      required: true,
+    },
+    productSize: {
+      type: String,
+      enum: ProductSize,
+      default: ProductSize.NORMAL,
+    },
+    productVaaolume: {
+      type: String,
+      enum: ProductVaolume,
+      default: ProductVaolume.ONE,
+    },
+    productDesc: {
+      type: String,
+      required: true,
+    },
+    productImage: {
+      type: [String],
+      default: [],
+    },
+    productViews: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } // Automatically adds createdAt and updatedAt
 );
 
+// Ensures a unique combination of productName, productSize, and productVolume
+productSchema.index(
+  { productName: 1, productSize: 1, productVaolume: 1 },
+  { unique: true }
+);
 
 export default mongoose.model("Product", productSchema);
