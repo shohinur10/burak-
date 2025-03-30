5cd09b1279eb0a8e78976d88509613f9d1776469
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-03-30 14:44:38 +0900

    feat : working on erum EAR

--- src/schema/Product.model.ts
+++ src/schema/Product.model.ts
@@ -1,2 +1,29 @@
+import mongoose, {Schema} from "mongoose";
+import { ProductCollection, ProductStatus } from '../libs/enums/product.enum';
 
 
+
+const productSchema = new Schema(
+{
+ productStatus: {
+    type: String,
+    enum: ProductCollection,
+    required: true,
+ },
+   productCollection: {
+    type:String ,
+    enum: ProductCollection,
+    required:true,
+
+ },
+
+productName:{
+    type:String,
+    required:true, 
+},
+},
+{ timestamps: true} // updatedAt, createdAt
+);
+
+
+export default mongoose.model("Product", productSchema);
--- src/views/train_class.ts
+++ src/views/train_class.ts
@@ -9,16 +9,31 @@
 
 */
 
+/* Request : 
+Traditional Api
+Rest Api 
+GraphQl Api
+*/
 
-/*
+
+/* Frontend Development;
 Traditional frontend development => BSSR (Adminka )=> EJS
 Modern frontend development => SPA =>( Users app) REACT library 
 */
- /*
+ /* Cookies:
   requist join
   desttoy all
   */
 
+
+
+
+
+   /* validation:
+   fromntend validation 
+   backend validtion
+   database validatio 
+   */
 /* bizlar birornarsanni  ejs da include qilganimizda  bitta  - qoyamiz 
 qiymati  kerak bosa = tenglik  qoyamiz 
 */
