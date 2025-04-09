66f86d420ec3a618dfcf38c59bd3ec3657c222b4
Shohinur <shohinur@ShohinucBookAir.lan>
2025-04-09 16:11:02 +0900

    feat : modify frontend upload process

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,66 +1,55 @@
+console.log("Signup frontend JavaScript file loaded");
 
-console.log("Signup frontend javascript file");
-$(function(){});
-
-$(function (){
+$(function () {
     const fileTarget = $(".file-box .upload-hidden");
-    let filename;
 
     fileTarget.on("change", function () {
-   if ( window.FileReader){
-    const uploadFile = $(this)[0].files[0];
-    const fileType =uploadFile["type"];
-    const validImageType =["image/jpg", "image/jpeg", "image/png"];
-    if (!validImageType.includes(fileType)){
-    alert("Please insert only jpeg, jpg, and png!");
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
 
-   } else {
-    if (uploadFile){
-        console.log(URL.createObjectURL(uploadFile));
-        $(".upload-img-frame")
-        .attr("src", URL.createObjectURL(uploadFile))
-        .addClass("success");
-    }
-    filename = $(this)[0].files[0].name;
- 
-   }
-   $(this).siblings(".upload-name").val(filename);
-}
+            if (uploadFile) {
+                const fileType = uploadFile.type;
+                const validImageTypes = ["image/jpg", "image/jpeg", "image/png"];
+
+                if (!validImageTypes.includes(fileType)) {
+                    alert("Please insert only jpeg, jpg, or png!");
+                } else {
+                    const imageUrl = URL.createObjectURL(uploadFile);
+                    console.log(imageUrl);
+
+                    $(".upload-img-frame")
+                        .attr("src", imageUrl)
+                        .addClass("success");
+
+                    const filename = uploadFile.name;
+                    $(this).siblings(".upload-name").val(filename);
+                }
+            }
+        }
     });
 });
 
-   
-
-function validateSignupForm(){
+function validateSignupForm() {
     const memberNick = $(".member-nick").val();
     const memberPhone = $(".member-phone").val();
     const memberPassword = $(".member-password").val();
     const confirmPassword = $(".confirm-password").val();
 
-
-
     if (
-        memberNick ==="" ||
-        memberPassword==="" ||
-        confirmPassword ===""||
-        memberPassword===""
-    ){
-        alert("Please insert all require inputs!"); 
+        memberNick === "" ||
+        memberPhone === "" ||
+        memberPassword === "" ||
+        confirmPassword === ""
+    ) {
+        alert("Please insert all required inputs!");
         return false;
     }
-if (memberPassword !== confirmPassword){
-    a;errorStrictEqual("Password differs, please check again!");
-    return false ;
+
+    if (memberPassword !== confirmPassword) {
+        alert("Password differs, please check again!");
+        return false;
+    }
+
+    return true;
 }
 
-
-
-
-const memberImage = $(".member-image").get(0).files[0].files? $(".member-image").get(0).
-files[0].name :null;
-
-if (!memberImage){
-    alert("Please insert restaurant image!");
-    return false;
-}
-}
