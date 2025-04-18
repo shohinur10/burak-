87596b621bd65bdbad67fabef85265948970ce97
Shohinur <shohinur@Shohinurui-MacBookAir.local>
2025-04-18 17:03:41 +0900

    feat: Task T

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,29 @@
+//Task T
+function mergeSortedArrays(arr1: number[], arr2: number[]): number[] {
+  const mergedArray: number[] = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      mergedArray.push(arr1[i]);
+      i++;
+    } else {
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    mergedArray.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    mergedArray.push(arr2[j]);
+    j++;
+  }
+  return mergedArray;
+}
+console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]));
+// => [0, 3, 4, 4, 6, 30, 31]
 // TASK S
 
 // Shunday function tuzing, u numberlardan tashkil topgan array qabul qilsin
